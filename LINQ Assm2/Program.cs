eef7400c52b8e66105bf2653b9dd838f76d5a784
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-08-16 19:02:43 +0300

    Filteration Operarors

--- LINQ Assm2/Program.cs
+++ LINQ Assm2/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace LINQ_Assm2
+﻿using ASSLINQ;
+using System.Collections;
+using static ASSLINQ.ListGenerator;
+
+
+namespace LINQ_Assm2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Filteration Operarors
+            //var Result = ProductList.Where(P => P.UnitsInStock == 0);
+
+            //Result = from P in ProductList
+            //         where P.UnitsInStock == 0
+            //         select P;
+
+            //var Result = ProductList.Where(P => { return P.Category == "Meat/Poultry" && P.UnitsInStock > 0; });
+
+            //Result = from p in ProductList
+            //         where p.UnitsInStock > 0 && p.Category == "Meat/Poultry"
+            //         select p;
+
+            //var Result = ProductList.Where((P, I) => P.UnitsInStock == 0 && I < 10);
+
+            //var Result = ProductList.Where((P, I) => I < 5 & P.UnitsInStock > 0);
+
+            //ArrayList list = new ArrayList() { "Ahmed" , 1 , "Hany" , 12 , "Makram" , 1.5 , 23 , 'A' };
+
+            //var Result = list.OfType<double>(); 
+
+
+            //foreach (var item in Result) 
+            //{ 
+            //    Console.WriteLine(item);
+            //} 
+            #endregion
+
+
+
+
         }
     }
 }
